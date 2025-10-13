ff9d686120f584f36c57a0f226a8b3234ce3da8e
jordanlak <jordanlak21@gmail.com>
2025-10-12 19:55:12 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+import pygame
+pygame.init()
+
+
+
+WIDTH = 600
+HEIGHT = 600
+screen = pyagme.dispaly.set_mode([WIDTH, HEIGHT])
+pygame.display.set_caption('2048')
+timer = pygame.time.Clock()
+fps - 60
+font = pygame.font.Font('
