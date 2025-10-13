c4597e75d3ba71a2e5a0f055dec5f0d6c8d1884d
jordanlak <jordanlak21@gmail.com>
2025-10-12 19:59:06 -0500

    Update main.py

--- main.py
+++ main.py
@@ -9,4 +9,10 @@ screen = pyagme.dispaly.set_mode([WIDTH, HEIGHT])
 pygame.display.set_caption('2048')
 timer = pygame.time.Clock()
 fps - 60
-font = pygame.font.Font('
+font = pygame.font.Font()
+
+                        
+run = True
+while run:
+  timer.tick(fps)
+  screen.fill('gray)
