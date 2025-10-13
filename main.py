a8d37c1addd239153723b086d0847f4c97f0e12f
jordanlak <jordanlak21@gmail.com>
2025-10-13 12:54:42 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 import pygame
 pygame.init()
-
+screen = pygame.display.set_mode((480, 360))
+clock = pygame.time.Clock()
+running = True
 
 
 WIDTH = 600
 HEIGHT = 600
-screen = pyagme.dispaly.set_mode([WIDTH, HEIGHT])
+screen = pyagme.display.set_mode([WIDTH, HEIGHT])
 pygame.display.set_caption('2048')
 timer = pygame.time.Clock()
 fps - 60
 font = pygame.font.Font()
+small_font = pygame.font.Font()
 
-                        
+                      
 run = True
 while run:
   timer.tick(fps)
-  screen.fill('gray)
+  screen.fill('pink)
+
+              
