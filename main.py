45b602adcb2e2a575917385d1fe7644a3a05fc63
jordanlak <jordanlak21@gmail.com>
2025-10-19 20:24:53 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
 import pygame
+import random
 pygame.init()
-window = pygame.display.set_mode((WIDTH, HEIGHT))
+
+WIDTH = 500
+HEIGHT 600
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('2048')
 screen = pygame.display.set_mode((600,500))
 clock = pygame.time.Clock()
+fps = 60
+font = pygame.font.Font('ariea'/ 15)
+
 running = True
   
 while runnig:
+  clock.tick(fps)
+  screen.fill('pink')
+  
 for event in pygame.event.get():
   if event.type == pygame.QUIT:
     running = False
-    break
 
-  pygame.QUIT()
-quit()
-def main():
-  run = True
-  clock = pygame.tick.Clock()
-  
+  pygame.display.flip()
+pygame.QUIT()
