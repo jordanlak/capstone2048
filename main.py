135193f32ae78b9d22b3b7703f23d3280dae4c74
jordanlak <jordanlak21@gmail.com>
2025-10-19 19:57:29 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,20 @@
 import pygame
-
-WIDTH = 500
-HEIGHT = 600
-fps = 60
-
 pygame.init()
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('2048')
+screen = pygame.display.set_mode((600,500))
+clock = pygame.time.Clock()
+running = True
+  
+while runnig:
+for event in pygame.event.get():
+  if event.type == pygame.QUIT:
+    running = False
+    break
 
+  pygame.QUIT()
+quit()
 def main():
   run = True
   clock = pygame.tick.Clock()
   
-while run:
-  clock.tick(fps)
-
-for event in pygame.event.get():
-  if event.type == pygame.QUIT:
-    run = False
-    break
-  pygame.QUIT()
-quit()
-
