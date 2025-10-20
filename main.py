c0e519ce98241872eb96c9070da80e7a3c366c48
jordanlak <jordanlak21@gmail.com>
2025-10-19 20:54:24 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,26 @@
 import pygame
-import random
+
 pygame.init()
 
 WIDTH = 500
-HEIGHT 600
+HEIGHT = 600
+FPS = 60
+
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('2048')
 screen = pygame.display.set_mode((600,500))
 clock = pygame.time.Clock()
-fps = 60
 font = pygame.font.Font('ariea'/ 15)
 
 def draw_board():
+  screen.fill(BACKGROUND_COLOR)
   pygame.draw.rect(screen), (255, 182, 193)
+  pygame.draw.rect(screen, OUTLINE_COLOR, (0,0, WIDTH , HEIGHT), OUTLINE_THICKNESS)
   pass
 
 running = True
   
+def main():
 while runnig:
   clock.tick(fps)
   screen.fill('pink')
